112051c0bf4fcc7ad7e81cb6701cd1849244c6a1
Christopher Wilhelm H. <info@kitsoft.at>
2024-01-20 00:40:34 +0100

    General update

--- src/main/java/eu/lotusgc/mc/event/ScoreboardHandler.java
+++ src/main/java/eu/lotusgc/mc/event/ScoreboardHandler.java
@@ -3,6 +3,8 @@ package eu.lotusgc.mc.event;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 import org.bukkit.Bukkit;
@@ -24,11 +26,11 @@ import org.bukkit.scoreboard.Team.OptionStatus;
 
 import eu.lotusgc.mc.ext.LotusController;
 import eu.lotusgc.mc.main.Main;
-import eu.lotusgc.mc.misc.InputType;
+import eu.lotusgc.mc.misc.ClearLag;
 import eu.lotusgc.mc.misc.MySQL;
 import eu.lotusgc.mc.misc.Playerdata;
 import eu.lotusgc.mc.misc.Prefix;
-import eu.lotusgc.mc.misc.Serverdata;
+import eu.lotusgc.mc.misc.ServerRestarter;
 import net.luckperms.api.model.user.User;
 import net.luckperms.api.model.user.UserManager;
 
@@ -55,18 +57,15 @@ public class ScoreboardHandler implements Listener{
 	
 	private static int sbSwitch = 0;
 	
-	public static void setScoreboard(Player player) {
+	public void setScoreboard(Player player) {
 		Scoreboard sb = Bukkit.getScoreboardManager().getNewScoreboard();
 		Objective o = sb.registerNewObjective("aaa", Criteria.DUMMY, "LGCINFOBOARD");
 		LotusController lc = new LotusController();
-		String currentUsersNetwork = lc.getServerData("BungeeCord", Serverdata.CurrentPlayers, InputType.Servername);
-		int currentUsersLocal = Bukkit.getOnlinePlayers().size();
-		String maxUsers = lc.getServerData("BungeeCord", Serverdata.MaxPlayers, InputType.Servername);
 		String sbPrefix = lc.getPrefix(Prefix.SCOREBOARD);
 		
 		o.setDisplaySlot(DisplaySlot.SIDEBAR);
 		sbSwitch++;
-		if(sbSwitch == 15) sbSwitch = 0; //resetting the Switcher to 0 so the views are going back again :)
+		if(sbSwitch == 10) sbSwitch = 0; //resetting the Switcher to 0 so the views are going back again :)
 		o.setDisplayName(sbPrefix);
 		int sbState = getSBState(player);
 		if(sbState == 0) {
@@ -75,42 +74,26 @@ public class ScoreboardHandler implements Listener{
 			//Player wont see a sideboard as well, however due to an error.
 			Main.logger.severe("Sideboard status reports code '-1' !");
 		}else if(sbState == 1) {
-			
-		}
-		if(sbSwitch >= 0 && sbSwitch <= 3) {
-			//money
-			o.getScore(lc.sendMessageToFormat(player, "event.scoreboard.money")).setScore(2);
-			o.getScore("§7» Pocket: §a" + lc.getPlayerData(player, Playerdata.MoneyPocket) + " §6Loti").setScore(1);
-			o.getScore("§7» Bank: §e" + lc.getPlayerData(player, Playerdata.MoneyBank) + " §6Loti").setScore(0);
-		}else if(sbSwitch >= 4 && sbSwitch <= 7) {
-			//role
-			o.getScore(lc.sendMessageToFormat(player, "event.scoreboard.role")).setScore(1);
-			o.getScore(retGroup(player)).setScore(0);
-		}else if(sbSwitch >= 8 && sbSwitch <= 11) {
-			//playerinfo
-			o.getScore(lc.sendMessageToFormat(player, "event.scoreboard.userid")).setScore(3);
-			o.getScore("§7» §a" + lc.getPlayerData(player, Playerdata.LotusChangeID)).setScore(2);
-			o.getScore("§7Clan:").setScore(1);
-			o.getScore("§7» §a" + lc.getPlayerData(player, Playerdata.Clan)).setScore(0);
-		}else if(sbSwitch >= 12 && sbSwitch <= 15) {
-			//serverinfo
-			//Will be reworked, as Lobby will have ViaVersion (to support 1.12.2 and latest (current 1.20.4)) - as the Hybrid Servers will be running mcv 1.12.2 - all HX servers will be listed on their own to just see servers where I can actually play on. (Website + Discord Bot will also be a possibility to lookup)
-			try {
-				PreparedStatement ps = MySQL.getConnection().prepareStatement("SELECT displayname,currentPlayers,isHiddenGame,isOnline FROM mc_serverstats ORDER BY serverid DESC");
-				ResultSet rs = ps.executeQuery();
-				int count = 0;
-				while(rs.next()) {
-					if(!rs.getBoolean("isHiddenGame") && rs.getBoolean("isOnline")) {
-						count++;
-						o.getScore("§7» " + rs.getString("displayname") + "§7: §f" + rs.getInt("currentPlayers")).setScore(count);
-					}
-				}
-				count++;
-				o.getScore("  §6" + currentUsersLocal + "§7, §a" + currentUsersNetwork + " §7/§c " + maxUsers).setScore(count);
-				o.getScore("§a§b").setScore(count + 1);
-				o.getScore("§7Servers").setScore(count + 2);
-			} catch (SQLException e) {
-				e.printStackTrace();
+			if(sbSwitch >= 0 && sbSwitch <= 5) {
+				//money
+				o.getScore(lc.sendMessageToFormat(player, "event.scoreboard.money")).setScore(5);
+				o.getScore("§7» Pocket: §a" + lc.getPlayerData(player, Playerdata.MoneyPocket) + " §6Loti").setScore(4);
+				o.getScore("§7» Bank: §e" + lc.getPlayerData(player, Playerdata.MoneyBank) + " §6Loti").setScore(3);
+				o.getScore("§f§4").setScore(2);
+				//role
+				o.getScore(lc.sendMessageToFormat(player, "event.scoreboard.role")).setScore(1);
+				o.getScore(retGroup(player)).setScore(0);
+			}else if(sbSwitch >= 6 && sbSwitch <= 10) {
+				//playerinfo (userid, clan, ping)
+				o.getScore(lc.sendMessageToFormat(player, "event.scoreboard.userid")).setScore(7);
+				o.getScore("§7» §a" + lc.getPlayerData(player, Playerdata.LotusChangeID)).setScore(6);
+				o.getScore("§f§c").setScore(5);
+				o.getScore("§7Clan:").setScore(4);
+				o.getScore("§7» §a" + lc.getPlayerData(player, Playerdata.Clan)).setScore(3);
+				o.getScore("§f§a").setScore(2);
+				o.getScore("§7Ping:").setScore(1);
+				o.getScore("§7» §a" + colorisePing(player.getPing())).setScore(0);
+				
 			}
 		}
 		player.setScoreboard(sb);
@@ -137,6 +120,20 @@ public class ScoreboardHandler implements Listener{
 		}else {
 			return -1;
 		}
+	}
+	
+	String colorisePing(int ping) {
+		String toReturn = "";
+		if(ping >= 0 && ping <= 99) {
+			toReturn = "§a" + ping + "§7ms";
+		}else if(ping >= 100 && ping <= 250) {
+			toReturn = "§e" + ping + "§7ms";
+		}else if(ping >= 251 && ping <= 400) {
+			toReturn = "§c" + ping + "§7ms";
+		}else if(ping >= 401) {
+			toReturn = "§4" + ping + "§7ms";
+		}
+		return toReturn;
 	}
 	
 	public Team getTeam(Scoreboard scoreboard, String role, ChatColor chatcolor) {
@@ -168,15 +165,33 @@ public class ScoreboardHandler implements Listener{
 			int count = 0;
 			while(rs.next()) {
 				count++;
-				tabHM.put(rs.getString("ingame-id"), rs.getString("colour") + rs.getString("short"));
-				chatHM.put(rs.getString("ingame-id"), rs.getString("colour") + rs.getString("name"));
-				roleHM.put(rs.getString("ingame-id"), rs.getString("priority"));
-				sbHM.put(rs.getString("ingame-id"), rs.getString("name"));
+				tabHM.put(rs.getString("ingame_id"), rs.getString("colour") + rs.getString("short"));
+				chatHM.put(rs.getString("ingame_id"), rs.getString("colour") + rs.getString("name"));
+				roleHM.put(rs.getString("ingame_id"), rs.getString("priority"));
+				sbHM.put(rs.getString("ingame_id"), rs.getString("name"));
 			}
 			Main.logger.info("Downloaded " + count + " roles for the Prefix System. | Source: ScoreboardHandler#initRoles();");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	//needs World#getTime()
+	private String parseTimeWorld(long time) {
+		long gameTime = time;
+		long hours = gameTime / 1000 + 6;
+		long minutes = (gameTime % 1000) * 60 / 1000;
+		String ampm = "AM";
+		if(hours >= 12) {
+			hours -= 12; ampm = "PM";
+		}
+		if(hours >= 12) {
+			hours -= 12; ampm = "AM";
+		}
+		if(hours == 0) hours = 12;
+		String mm = "0" + minutes;
+		mm = mm.substring(mm.length() - 2, mm.length());
+		return hours + ":" + mm + " " + ampm;
 	}
 	
 	private String returnPrefix(String role, RankType type) {
@@ -204,7 +219,7 @@ public class ScoreboardHandler implements Listener{
 		TEAM
 	}
 	
-	public static void startScheduler(int delay, int sideboardRefresh, int tabRefresh) {
+	public void startScheduler(int delay, int sideboardRefresh, int tabRefresh) {
 		//SYNC TASK - ONLY FOR THE SIDEBOARD
 		new BukkitRunnable() {
 			@Override
@@ -219,9 +234,12 @@ public class ScoreboardHandler implements Listener{
 			@Override
 			public void run() {
 				LotusController lc = new LotusController();
+				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 				for(Player all : Bukkit.getOnlinePlayers()) {
-					all.setPlayerListHeaderFooter("§cLotus §aGaming §fCommunity", "§7Server: §a" + lc.getServerName() + "\n§7Time: §a00:00\n§7Ping: §a" + all.getPing());
+					all.setPlayerListHeaderFooter("§cLotus §aGaming §fCommunity", "§7Server: §a" + lc.getServerName() + "\n§7Time: §a" + sdf.format(new Date()) + "\n§7Ping: §a" + all.getPing());
 				}
+				new ServerRestarter().triggerRestart();
+				new ClearLag().triggerClearlag();
 			}
 		}.runTaskTimerAsynchronously(Main.main, delay, tabRefresh);
 	}
