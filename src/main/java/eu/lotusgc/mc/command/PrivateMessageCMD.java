111623d14b52c4fb9474ecab791ca1863902f8ed
Christopher Wilhelm H. <info@kitsoft.at>
2024-01-22 04:04:10 +0100

    Start of PM System (paused due to Survey)

---
+++ src/main/java/eu/lotusgc/mc/command/PrivateMessageCMD.java
@@ -0,0 +1,30 @@
+package eu.lotusgc.mc.command;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import eu.lotusgc.mc.ext.LotusController;
+import eu.lotusgc.mc.main.Main;
+
+public class PrivateMessageCMD implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(!(sender instanceof Player)) {
+			Bukkit.getConsoleSender().sendMessage(Main.consoleSend);
+		}else {
+			Player player = (Player) sender;
+			LotusController lc = new LotusController();
+			if(command.getName().equalsIgnoreCase("msg")) {
+				
+			}else if(command.getName().equalsIgnoreCase("r")) {
+				
+			}
+		}
+		return false;
+	}
+
+}
