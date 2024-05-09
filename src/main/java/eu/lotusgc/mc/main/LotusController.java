d41915f08a790216e34e671c53494d0946806368
Maurice H. <development@lotuscommunity.eu>
2024-05-09 17:06:36 +0200

    Added CPU Load Calculator to API

--- src/main/java/eu/lotusgc/mc/main/LotusController.java
+++ src/main/java/eu/lotusgc/mc/main/LotusController.java
@@ -3,6 +3,7 @@ package eu.lotusgc.mc.main;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.lang.management.ManagementFactory;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.sql.PreparedStatement;
@@ -624,6 +625,33 @@ public class LotusController {
 			afkPlayers.add(player.getUniqueId());
 		}else {
 			if(afkPlayers.contains(player.getUniqueId())) afkPlayers.remove(player.getUniqueId());
+		}
+	}
+	
+	static long lastSystemTime = 0;
+	static long lastProcessCpuTime = 0;
+	static int availableProcessors = ManagementFactory.getOperatingSystemMXBean().getAvailableProcessors();
+	
+	public synchronized double getCpuUsage() {
+		if(lastSystemTime == 0) {
+			baselineCounters();
+		}
+		long systemTime = System.nanoTime();
+		long processCpuTime = 0;
+		
+		if(ManagementFactory.getOperatingSystemMXBean() instanceof com.sun.management.OperatingSystemMXBean) {
+			processCpuTime = ((com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean()).getProcessCpuTime();
+		}
+		double cpuUsage = (double) (processCpuTime - lastProcessCpuTime) / (systemTime - lastSystemTime)*100.0;
+		lastSystemTime = systemTime;
+		lastProcessCpuTime = processCpuTime;
+		return cpuUsage / availableProcessors;
+	}
+	
+	private void baselineCounters() {
+		lastSystemTime = System.nanoTime();
+		if(ManagementFactory.getOperatingSystemMXBean() instanceof com.sun.management.OperatingSystemMXBean) {
+			lastProcessCpuTime = ((com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean()).getProcessCpuTime();
 		}
 	}
 }
